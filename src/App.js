4d264960c106af4a7a045741da93b236f0f242fc
Abdenour TAKHERBOUCHT <takher.abd@gmail.com>
2019-05-08 17:03:53 +0200

    Add BOX component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import "./App.css";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -12,19 +13,27 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const classes = useStyles();
   return (
-    <div className="App">
-      <h1>Azul fell-awen</h1>
-      <h1>Bonjour tout le monde</h1>
-      <Button variant="outlined" className={classes.button}>
-        Default
-      </Button>
-      <Button variant="outlined" color="primary" className={classes.button}>
-        Primary
-      </Button>
-      <Button variant="outlined" color="secondary" className={classes.button}>
-        Secondary
-      </Button>
-    </div>
+    <Box border={1} borderColor="primary.main" p={4}>
+      <Typography component="div">
+        <Box border={1} borderColor="grey.200" fontSize="h6.fontSize" textAlign="left" m={1}>
+          Azul Fell-awen akk!
+        </Box>
+        <Box textAlign="center" m={1}>
+          Bonjour tout le monde!
+        </Box>
+      </Typography>
+      <Box display="flex" justifyContent="space-between" m={1} p={1} bgcolor="background.paper">
+        <Button variant="outlined" className={classes.button}>
+          Default
+        </Button>
+        <Button variant="outlined" color="primary" className={classes.button}>
+          Primary
+        </Button>
+        <Button variant="outlined" color="secondary" className={classes.button}>
+          Secondary
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
