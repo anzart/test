35a6e7d0ce8c5feac615a6d3a54917f9916183ef
Abdenour TAKHERBOUCHT <takher.abd@gmail.com>
2019-05-08 15:54:08 +0200

    Add MaterialUI package, and MaterialUI Buttons components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import "./App.css";
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1)
+  }
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <h1>Azul fell-awen</h1>
       <h1>Bonjour tout le monde</h1>
+      <Button variant="outlined" className={classes.button}>
+        Default
+      </Button>
+      <Button variant="outlined" color="primary" className={classes.button}>
+        Primary
+      </Button>
+      <Button variant="outlined" color="secondary" className={classes.button}>
+        Secondary
+      </Button>
     </div>
   );
 }
